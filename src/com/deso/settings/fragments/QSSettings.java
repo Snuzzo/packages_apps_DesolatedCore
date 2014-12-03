f6bc0837d94a74c488369da430fb3e70cc5aecc9
Lars Greiss <kufikugel@googlemail.com>
2014-12-03 20:13:44 +0100

    Return: Smart Pulldown [2/2]
    Rewritten for 6.0/7.0 by @Beanstown106
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/deso/settings/fragments/QSSettings.java
+++ src/com/deso/settings/fragments/QSSettings.java
@@ -56,6 +56,7 @@ public class QSSettings extends DesoSettingsFragment implements
     private static final String CUSTOM_HEADER_PROVIDER = "custom_header_provider";
     private static final String CUSTOM_HEADER_BROWSE = "custom_header_browse";
     private static final String QUICK_PULLDOWN = "quick_pulldown";
+    private static final String PREF_SMART_PULLDOWN = "smart_pulldown";
 
     private CustomSeekBarPreference mSysuiQqsCount;
     private ListPreference mWeatherIconPack;
@@ -66,6 +67,7 @@ public class QSSettings extends DesoSettingsFragment implements
     private String mDaylightHeaderProvider;
     private PreferenceScreen mHeaderBrowse;
     private ListPreference mQuickPulldown;
+    private ListPreference mSmartPulldown;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -162,6 +164,13 @@ public class QSSettings extends DesoSettingsFragment implements
                 Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 1, UserHandle.USER_CURRENT);
         mQuickPulldown.setValue(String.valueOf(quickPulldownValue));
         updatePulldownSummary(quickPulldownValue);
+
+        mSmartPulldown = (ListPreference) findPreference(PREF_SMART_PULLDOWN);
+        mSmartPulldown.setOnPreferenceChangeListener(this);
+        int smartPulldown = Settings.System.getInt(getContentResolver(),
+                Settings.System.QS_SMART_PULLDOWN, 0);
+        mSmartPulldown.setValue(String.valueOf(smartPulldown));
+        updateSmartPulldownSummary(smartPulldown);
     }
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == mSysuiQqsCount) {
@@ -202,8 +211,29 @@ public class QSSettings extends DesoSettingsFragment implements
                     quickPulldownValue, UserHandle.USER_CURRENT);
             updatePulldownSummary(quickPulldownValue);
             return true;
-         }
+        } else if (preference == mSmartPulldown) {
+            int smartPulldown = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(), Settings.System.QS_SMART_PULLDOWN, smartPulldown);
+            updateSmartPulldownSummary(smartPulldown);
+            return true;
+        }
         return false;
+   }
+
+   private void updateSmartPulldownSummary(int value) {
+        Resources res = getResources();
+
+        if (value == 0) {
+            // Smart pulldown deactivated
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_off));
+        } else if (value == 3) {
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_none_summary));
+        } else {
+            String type = res.getString(value == 1
+                    ? R.string.smart_pulldown_dismissable
+                    : R.string.smart_pulldown_ongoing);
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_summary, type));
+        }
     }
 
     private boolean isOmniJawsServiceInstalled() {
@@ -291,7 +321,7 @@ public class QSSettings extends DesoSettingsFragment implements
                     ? R.string.quick_pulldown_left
                     : R.string.quick_pulldown_right);
             mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_summary, direction));
-       }
+        }
     }
 
     @Override
