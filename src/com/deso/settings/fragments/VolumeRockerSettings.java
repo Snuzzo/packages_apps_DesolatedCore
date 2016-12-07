cf252179ceb27c6ee244768b8fb40d3897199265
ezio84 <brabus84@gmail.com>
2016-12-07 18:16:45 +0100

    Forward port 'Swap volume buttons' (3/3)

--- src/com/deso/settings/fragments/VolumeRockerSettings.java
+++ src/com/deso/settings/fragments/VolumeRockerSettings.java
@@ -18,17 +18,41 @@ package com.deso.settings.fragments;
 
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import android.support.v14.preference.SwitchPreference;
 
 import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
 
-public class VolumeRockerSettings extends DesoSettingsFragment {
+public class VolumeRockerSettings extends DesoSettingsFragment implements Preference.OnPreferenceChangeListener {
+
+    private static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
+    private SwitchPreference mSwapVolumeButtons;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         title = getResources().getString(R.string.volume_rocker_settings_title);
         addPreferencesFromResource(R.xml.volumerocker_settings);
+
+        // volume rocker reorient
+        mSwapVolumeButtons = (SwitchPreference) findPreference(SWAP_VOLUME_BUTTONS);
+        mSwapVolumeButtons.setOnPreferenceChangeListener(this);
+        int swapVolumeButtons = Settings.System.getInt(getContentResolver(),
+                SWAP_VOLUME_BUTTONS, 0);
+        mSwapVolumeButtons.setChecked(swapVolumeButtons != 0);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mSwapVolumeButtons) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), SWAP_VOLUME_BUTTONS,
+                    value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 
 }
