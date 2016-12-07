6ea9bd4a8486f93a55b2b4b4ee488228ec1a7a17
Austin T. Conn <>
2016-12-07 18:23:23 +0100

    Volume Rocker music controls [2/2]

--- src/com/deso/settings/fragments/VolumeRockerSettings.java
+++ src/com/deso/settings/fragments/VolumeRockerSettings.java
@@ -30,9 +30,11 @@ public class VolumeRockerSettings extends DesoSettingsFragment implements Prefer
 
     private static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
     private static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+    private static final String VOLUME_ROCKER_MUSIC_CONTROLS = "volume_rocker_music_controls";
 
     private SwitchPreference mSwapVolumeButtons;
     private SwitchPreference mVolumeRockerWake;
+    private SwitchPreference mVolumeRockerMusicControl;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,8 +55,16 @@ public class VolumeRockerSettings extends DesoSettingsFragment implements Prefer
         int volumeRockerWake = Settings.System.getInt(getContentResolver(),
                 VOLUME_ROCKER_WAKE, 0);
         mVolumeRockerWake.setChecked(volumeRockerWake != 0);
+
+        // volume rocker music control
+        mVolumeRockerMusicControl = (SwitchPreference) findPreference(VOLUME_ROCKER_MUSIC_CONTROLS);
+        mVolumeRockerMusicControl.setOnPreferenceChangeListener(this);
+        int volumeRockerMusicControl = Settings.System.getInt(getContentResolver(),
+                VOLUME_ROCKER_MUSIC_CONTROLS, 0);
+        mVolumeRockerMusicControl.setChecked(volumeRockerMusicControl != 0);
     }
 
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == mSwapVolumeButtons) {
             boolean value = (Boolean) newValue;
@@ -64,6 +74,11 @@ public class VolumeRockerSettings extends DesoSettingsFragment implements Prefer
         } else if (preference == mVolumeRockerWake) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getContentResolver(), VOLUME_ROCKER_WAKE,
+                    value ? 1 : 0);
+            return true;
+        } else if (preference == mVolumeRockerMusicControl) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), VOLUME_ROCKER_MUSIC_CONTROLS,
                     value ? 1 : 0);
             return true;
         }
