284731376402216251d189934c4eabf995304ba2
xyyx <xyyx@mail.ru>
2016-12-07 16:04:53 +0100

    Custom small QS tiles [2/2]
    edit by Mazda-- use custom seekbar instead of list

--- src/com/deso/settings/DesoSettings.java
+++ src/com/deso/settings/DesoSettings.java
@@ -49,7 +49,7 @@ import com.deso.settings.fragments.PowerMenuSettings;
 import com.deso.settings.fragments.StatusBarSettings;
 import com.deso.settings.fragments.NavbarSettings;
 import com.deso.settings.fragments.RecentsSettings;
-//import com.deso.settings.fragments.QSSettings;
+import com.deso.settings.fragments.QSSettings;
 import com.deso.settings.fragments.VolumeRockerSettings;
 import com.deso.settings.fragments.MiscSettings;
 //import com.deso.settings.fragments.AboutDeso;
@@ -107,7 +107,7 @@ public class DesoSettings extends SettingsPreferenceFragment {
             frags.add((DesoSettingsFragment) new LockscreenSettings());
             frags.add((DesoSettingsFragment) new NavbarSettings());
             frags.add((DesoSettingsFragment) new RecentsSettings());
-            //frags.add((DesoSettingsFragment) new QSSettings());
+            frags.add((DesoSettingsFragment) new QSSettings());
             frags.add((DesoSettingsFragment) new VolumeRockerSettings());
             frags.add((DesoSettingsFragment) new MiscSettings());
             //frags.add((DesoSettingsFragment) new AboutDeso());
---
+++ src/com/deso/settings/fragments/QSSettings.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2017 Desolation ROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.deso.settings.fragments;
+
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.deso.settings.preferences.CustomSeekBarPreference;
+
+public class QSSettings extends DesoSettingsFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
+
+    private CustomSeekBarPreference mSysuiQqsCount;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        title = getResources().getString(R.string.qs_settings_title);
+        addPreferencesFromResource(R.xml.qs_settings);
+
+        mSysuiQqsCount = (CustomSeekBarPreference) findPreference(KEY_SYSUI_QQS_COUNT);
+        int SysuiQqsCount = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.QQS_COUNT, 5);
+        mSysuiQqsCount.setValue(SysuiQqsCount / 1);
+        mSysuiQqsCount.setOnPreferenceChangeListener(this);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mSysuiQqsCount) {
+            int SysuiQqsCount = (Integer) newValue;
+            Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.QQS_COUNT, SysuiQqsCount * 1);
+            return true;
+        }
+        return false;
+    }
+}
