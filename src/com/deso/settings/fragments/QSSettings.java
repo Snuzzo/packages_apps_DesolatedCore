48baf48160f18c648e93260dffcf7624e2cf626d
maxwen <max.weninger@gmail.com>
2017-01-10 16:10:32 +0800

    [2/3]QuickSettings: custom header extensions static image provider call BrowseHeaderActivity from OmniStyle
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/deso/settings/fragments/QSSettings.java
+++ src/com/deso/settings/fragments/QSSettings.java
@@ -34,6 +34,7 @@ import android.support.v14.preference.SwitchPreference;
 import com.android.settings.R;
 import com.android.internal.util.tesla.TeslaUtils;
 import com.deso.settings.preferences.CustomSeekBarPreference;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -50,12 +51,17 @@ public class QSSettings extends DesoSettingsFragment implements
     private static final String DAYLIGHT_HEADER_PACK = "daylight_header_pack";
     private static final String DEFAULT_HEADER_PACKAGE = "com.android.systemui";
     private static final String CUSTOM_HEADER_IMAGE_SHADOW = "status_bar_custom_header_shadow";
+    private static final String CUSTOM_HEADER_PROVIDER = "custom_header_provider";
+    private static final String CUSTOM_HEADER_BROWSE = "custom_header_browse";
 
     private CustomSeekBarPreference mSysuiQqsCount;
     private ListPreference mWeatherIconPack;
     private PreferenceCategory mWeatherCategory;
     private ListPreference mDaylightHeaderPack;
     private CustomSeekBarPreference mHeaderShadow;
+    private ListPreference mHeaderProvider;
+    private String mDaylightHeaderProvider;
+    private PreferenceScreen mHeaderBrowse;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -101,6 +107,22 @@ public class QSSettings extends DesoSettingsFragment implements
         mHeaderShadow.setValue(headerShadow);
         mHeaderShadow.setOnPreferenceChangeListener(this);
 
+        mDaylightHeaderProvider = getResources().getString(R.string.daylight_header_provider);
+        String providerName = Settings.System.getString(getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER_PROVIDER);
+        if (providerName == null) {
+            providerName = mDaylightHeaderProvider;
+        }
+        mHeaderProvider = (ListPreference) findPreference(CUSTOM_HEADER_PROVIDER);
+        valueIndex = mHeaderProvider.findIndexOfValue(providerName);
+        mHeaderProvider.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+        mHeaderProvider.setSummary(mHeaderProvider.getEntry());
+        mHeaderProvider.setOnPreferenceChangeListener(this);
+        mDaylightHeaderPack.setEnabled(providerName.equals(mDaylightHeaderProvider));
+
+        mHeaderBrowse = (PreferenceScreen) findPreference(CUSTOM_HEADER_BROWSE);
+        mHeaderBrowse.setEnabled(isBrowseHeaderAvailable());
+
         mWeatherCategory = (PreferenceCategory) findPreference(CATEGORY_WEATHER);
         if (mWeatherCategory != null && !isOmniJawsServiceInstalled()) {
             getPreferenceScreen().removePreference(mWeatherCategory);
@@ -138,16 +160,24 @@ public class QSSettings extends DesoSettingsFragment implements
                     Settings.Secure.QQS_COUNT, SysuiQqsCount * 1);
             return true;
         } else if (preference == mDaylightHeaderPack) {
-            String value = (String) objValue;
+            String value = (String) newValue;
             Settings.System.putString(getContentResolver(),
                     Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK, value);
             int valueIndex = mDaylightHeaderPack.findIndexOfValue(value);
             mDaylightHeaderPack.setSummary(mDaylightHeaderPack.getEntries()[valueIndex]);
             return true;
          } else if (preference == mHeaderShadow) {
-            Integer headerShadow = (Integer) objValue;
+            Integer headerShadow = (Integer) newValue;
             Settings.System.putInt(getContentResolver(),
                     Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW, headerShadow);
+            return true;
+        } else if (preference == mHeaderProvider) {
+            String value = (String) newValue;
+            Settings.System.putString(getContentResolver(),
+                    Settings.System.STATUS_BAR_CUSTOM_HEADER_PROVIDER, value);
+            int valueIndex = mHeaderProvider.findIndexOfValue(value);
+            mHeaderProvider.setSummary(mHeaderProvider.getEntries()[valueIndex]);
+            mDaylightHeaderPack.setEnabled(value.equals(mDaylightHeaderProvider));
             return true;
         } else if (preference == mWeatherIconPack) {
             String value = (String) newValue;
@@ -230,5 +260,17 @@ public class QSSettings extends DesoSettingsFragment implements
             }
             entries.add(label);
         }
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.DESO;
+    }
+
+    private boolean isBrowseHeaderAvailable() {
+        PackageManager pm = getPackageManager();
+        Intent browse = new Intent();
+        browse.setClassName("org.omnirom.omnistyle", "org.omnirom.omnistyle.BrowseHeaderActivity");
+        return pm.resolveActivity(browse, 0) != null;
     }
 }
