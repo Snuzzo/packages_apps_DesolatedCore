2b97e19c9a1c2c90b6a6e81230be755896014508
Jacob McSwain <jacob.a.mcswain@gmail.com>
2017-05-13 03:37:49 -0500

    Notification Count in Statusbar [2/2]

---
+++ src/com/deso/settings/fragments/MiscSettings.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2017 Desolation ROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.deso.settings.fragments;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class LockscreenSettings extends DesoSettingsFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        title = getResources().getString(R.string.misc_settings_title);
+        addPreferencesFromResource(R.xml.misc_settings);
+    }
+
+}
