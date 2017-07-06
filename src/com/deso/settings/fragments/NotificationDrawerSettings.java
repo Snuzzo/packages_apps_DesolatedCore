Cosmic-OS/platform_packages_apps_Galaxy

Add Notification Drawer Fragment
Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

src/com/cosmic/settings/fragments/NotificationDrawerSettings.java
@@ -1,0 +1,41 @@
+/*
+ * Copyright (C) 2016 Cosmic-OS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cosmic.settings.fragments;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class NotificationDrawerSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.notification_drawer_settings);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.GALAXY;
+    }
+}
+

Snuzzo/packages_apps_DesolatedCore

src/com/deso/settings/fragments/NotificationDrawerSettings.java
@@ -1,0 +1,42 @@
+/*
+ * Copyright (C) 2016 Cosmic-OS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cosmic.settings.fragments;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class NotificationDrawerSettings extends DesoSettingsFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.notification_drawer_settings);
+        title = getResources().getString(R.string.notification_drawer_settings_title);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.DESO;
+    }
+}
+