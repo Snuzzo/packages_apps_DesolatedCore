120ffb316293a236707123dd8d8d76614a538aa5
Snuzzo <jrracinfan@gmail.com>
2017-07-10 14:03:33 -0400

    Immersive Recents 2/2

--- src/com/deso/settings/fragments/RecentsSettings.java
+++ src/com/deso/settings/fragments/RecentsSettings.java
@@ -16,13 +16,31 @@
 
 package com.deso.settings.fragments;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.app.Fragment;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
-public class RecentsSettings extends DesoSettingsFragment {
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+public class RecentsSettings extends DesoSettingsFragment Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,6 +48,28 @@ public class RecentsSettings extends DesoSettingsFragment {
         title = getResources().getString(R.string.recents_settings_title);
         addPreferencesFromResource(R.xml.recents_settings);
         final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mImmersiveRecents = (ListPreference) findPreference(IMMERSIVE_RECENTS);
+        mImmersiveRecents.setValue(String.valueOf(Settings.System.getInt(
+                getContentResolver(), Settings.System.IMMERSIVE_RECENTS, 0)));
+        mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
+        mImmersiveRecents.setOnPreferenceChangeListener(this);
     }
 
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mImmersiveRecents) {
+            Settings.System.putInt(getContentResolver(), Settings.System.IMMERSIVE_RECENTS,
+                    Integer.valueOf((String) newValue));
+            mImmersiveRecents.setValue(String.valueOf(newValue));
+            mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.DESO;
+    }
 }
