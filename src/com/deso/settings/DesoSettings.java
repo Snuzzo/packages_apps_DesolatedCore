ebb411cb1fab3a3373d43fdb7086ff8ce43af1c0
Snuzzo <jrracinfan@gmail.com>
2017-08-02 16:25:57 -0400

    Refactor part 1

--- src/com/deso/settings/DesoSettings.java
+++ src/com/deso/settings/DesoSettings.java
@@ -17,116 +17,20 @@
 
 package com.deso.settings;
 
-
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.LayerDrawable;
-import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.view.PagerTabStrip;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.preference.PreferenceScreen;
-import android.support.v7.widget.Toolbar;
-import android.support.v13.app.FragmentPagerAdapter;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-
-import com.deso.settings.fragments.ClockSettings;
-import com.deso.settings.fragments.DesoSettingsFragment;
-import com.deso.settings.fragments.LedSettings;
-import com.deso.settings.fragments.LockscreenSettings;
-import com.deso.settings.fragments.PowerMenuSettings;
-import com.deso.settings.fragments.StatusBarSettings;
-import com.deso.settings.fragments.NavbarSettings;
-import com.deso.settings.fragments.NotificationDrawerSettings;
-import com.deso.settings.fragments.RecentsSettings;
-import com.deso.settings.fragments.QSSettings;
-import com.deso.settings.fragments.MiscSettings;
-//import com.deso.settings.fragments.AboutDeso;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
+
 import com.android.settings.SettingsPreferenceFragment;
 
 public class DesoSettings extends SettingsPreferenceFragment {
 
-    ViewPager mViewPager;
-    ViewGroup mContainer;
-    PagerTabStrip mTabs;
-    SectionsPagerAdapter mSectionsPagerAdapter;
-
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        mContainer = container;
-
-        View view = inflater.inflate(R.layout.main_layout, container, false);
-        mViewPager = (ViewPager) view.findViewById(R.id.pager);
-        mTabs = (PagerTabStrip) view.findViewById(R.id.tabs);
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        mViewPager.setCurrentItem(1);
-        mViewPager.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                mViewPager.setCurrentItem(0);
-            }
-        },100);
-        setHasOptionsMenu(true);
-        return view;
-    }
-
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle saveState) {
-        super.onSaveInstanceState(saveState);
-    }
-
-    class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        private ArrayList<DesoSettingsFragment> frags = new ArrayList<DesoSettingsFragment>();
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-            frags.add((DesoSettingsFragment) new StatusBarSettings());
-            frags.add((DesoSettingsFragment) new NotificationDrawerSettings());
-            if(DesoSettings.this.getResources().getBoolean(com.android.internal.R.bool.config_intrusiveBatteryLed) ||
-               DesoSettings.this.getResources().getBoolean(com.android.internal.R.bool.config_intrusiveNotificationLed))
-               frags.add((DesoSettingsFragment) new LedSettings());
-            frags.add((DesoSettingsFragment) new PowerMenuSettings());
-            frags.add((DesoSettingsFragment) new LockscreenSettings());
-            frags.add((DesoSettingsFragment) new NavbarSettings());
-            frags.add((DesoSettingsFragment) new RecentsSettings());
-            frags.add((DesoSettingsFragment) new QSSettings());
-            frags.add((DesoSettingsFragment) new MiscSettings());
-            //frags.add((DesoSettingsFragment) new AboutDeso());
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return (Fragment) frags.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return frags.size();
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return ((DesoSettingsFragment) frags.get(position)).getTitle();
-        }
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.deso_settings_main);
+        PreferenceScreen prefScreen = getPreferenceScreen();
     }
 
     @Override
